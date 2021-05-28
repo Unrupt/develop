091a08b1ef580488216f000fcacb6d2804f5d34f
James Reynolds <jameshreynolds@gmail.com>
2021-05-28 08:41:17 -0700

    Update properties.js

--- properties.js
+++ properties.js
@@ -6,16 +6,16 @@ var properties = {
     procFramesize: 4096, // how many samples (at 44k1 Hz) in a frame. default 4096 ->  ~100ms
                          // this impacts the latency - but go too low and the audio will break up
     scopeFftSize : 2048, // number of samples in the FFT for the oscilloscope. default 2048 seems ok
-    micSilenceThreshold : 0.015, // minumum mean mic volume (in range of 0.0->1.0) of a frame that contains voice
+    micSilenceThreshold : 0.1, // minumum mean mic volume (in range of 0.0->1.0) of a frame that contains voice
                                   // used to trigger pause/unpause
                                   // default 0.0175 ok on imacs
-    farSilenceThreshold : 0.015, // minimum mean remote volume (in range of 0.0->1.0) of a frame that contains voice
+    farSilenceThreshold : 0.1, // minimum mean remote volume (in range of 0.0->1.0) of a frame that contains voice
                                   // used to trim silence from playout
                                   // default 0.0175 ok on imacs
 
     useAudioOnlyLayout: false, // If true then the audio layout should be used instead of the main video/audio layout.
-    minFramesSilenceForPause: 5, // number of non-silent frames before we pause. default 3 (300ms)
-    minFramesSilenceForPlay: 5,  // number of silent frames before we start to clip silence from playback. default 3 (300ms)
+    minFramesSilenceForPause: 3, // number of non-silent frames before we pause. default 3 (300ms)
+    minFramesSilenceForPlay: 3,  // number of silent frames before we start to clip silence from playback. default 3 (300ms)
     maxStashFrames: 500, // longest possible pause (in frames). default 1000 -> 100 sec
     websocketURL: "wss://pi.pe/websocket/?finger=" // where to find the redezvous server.
 };
